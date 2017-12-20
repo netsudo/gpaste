baa842c3c88e706de43d326c93667f19793a99b8
netsudo <netsudo@users.noreply.github.com>
2017-12-20 14:28:05 -0800

    Simplest version of page to test with

--- gpaste/paste/views.py
+++ gpaste/paste/views.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from paste import app, db
 from paste.models import Post
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html')
