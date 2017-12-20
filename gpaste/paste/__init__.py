74e0f319ad6df7d326d68b45cfda591c6dbe0068
netsudo <netsudo@users.noreply.github.com>
2017-12-19 18:30:58 -0800

    Setup the DB and added first simple model field

--- gpaste/paste/__init__.py
+++ gpaste/paste/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://test:password@localhost/Paste'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 import paste.views, paste.models
--- gpaste/paste/models.py
+++ gpaste/paste/models.py
@@ -1,1 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
+from paste import db
+from datetime import datetime
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Date %r> <Content %r>' % (self.pub_date, self.content)
