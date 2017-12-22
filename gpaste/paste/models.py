fa8dc1221077442213d504f1cb2668166f6597c5
netsudo <netsudo@users.noreply.github.com>
2017-12-21 21:34:12 -0800

    Added the language field to the models to determine highlighting later on and the 404 and layout templates

--- gpaste/paste/models.py
+++ gpaste/paste/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
+    language_highlight = db.Column(db.Text, nullable=False)
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
