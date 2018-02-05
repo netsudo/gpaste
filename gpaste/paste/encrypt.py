6fc41bc24db97d7b2c60e5f4fa7a84e30741b326
netsudo <netsudo@users.noreply.github.com>
2018-02-04 23:33:43 -0800

    Added the unencrypt method and turned the encrypt module into a class

--- gpaste/paste/encrypt.py
+++ gpaste/paste/encrypt.py
@@ -1,18 +1,29 @@
 import os
 import base64
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.backends import default_backend
 
-def encryptContent(content, password):
-    backend = default_backend()
-    salt = os.urandom(16)
+class Encrypt:
+    def __fernetKey(self, salt, password):
+        backend = default_backend()
 
-    kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1, backend=backend)
-    key = base64.urlsafe_b64encode(kdf.derive(b"%a" % password))
+        kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1, backend=backend)
+        key = base64.urlsafe_b64encode(kdf.derive(b"%a" % password))
 
-    f = Fernet(key)
-    token = f.encrypt(b"%a" % content)
+        return Fernet(key)
 
-    return salt, token
+    def encryptContent(self, content, password):
+        salt = os.urandom(16)
+        f = self.__fernetKey(salt, password)
+        token = f.encrypt(b"%a" % content)
 
+        return salt, token
+
+    def unencryptContent(self, salt, token, password):
+        f = self.__fernetKey(salt, password)
+
+        try:
+            return f.decrypt(token)
+        except InvalidToken:
+            return False
